565ee680ca9ba38872573fff856c6b1709be1248
Jay <lijiakof@gmail.com>
2018-11-12 11:54:02 +0800

    Build to iife module

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,14 +1,28 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'src/index.js',
     plugins: [
+        resolve({
+            browser: true,
+            jsnext: true,
+            main: true,
+            preferBuiltins: false
+        }),
+        commonjs({
+            //include: 'node_modules/**'
+        }),
+        json(),
         babel({
             exclude: 'node_modules/**'
         })
     ],
     output: {
         file: 'dist/index.js',
-        format: 'cjs'
+        name: 'Tripio',
+        format: 'iife' //cjs | umd
     }
 };

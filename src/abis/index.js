bb47c76294c320f1a0ab00183303383428ff1b32
Jay <lijiakof@gmail.com>
2018-11-13 19:25:14 +0800

    Add abi module

---
+++ src/abis/index.js
@@ -0,0 +1,5 @@
+import RoomNightAdmin from '../abis/roomnight-admin';
+import RoomNightCustomer from '../abis/roomnight-customer';
+import RoomNightVendor from '../abis/roomnight-vendor';
+
+export { RoomNightAdmin, RoomNightCustomer, RoomNightVendor};
